b9e4d13920afeba450c23c632f9143b2115d3859
Jomari Gee <mrjamgee@gmail.com>
2022-02-12 15:19:02 -0500

    Update Config.sh

--- Config.sh
+++ Config.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-#One-liner to start up the TryHackMe VPN Configuration
+#Start up the TryHackMe VPN Configuration
 
 #Home Directory, OpenVPN Config File, start openvpn
 cd ~ && cd THM/OpenVPN && sudo openvpn config.ovpn 
